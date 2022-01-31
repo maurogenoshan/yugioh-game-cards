bf65f098f565b0fbbac2737fdb4308a9c44bf7a1
maurogaitan <maurogaitansouvaje@gmail.com>
2022-01-31 01:52:55 -0300

    Protejo rutas y agrego prefijo

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,11 @@ use App\Http\Controllers\API\CardsController;
 |--------------------------------------------------------------------------
 */
 
-Route::post('login', [AuthController::class, 'signin']);
-Route::post('register', [AuthController::class, 'signup']);
-Route::resource('cards', CardsController::class);
-Route::middleware('auth:sanctum')->group(function () {
+
+
+
+Route::group(['prefix' => 'api/v1', 'middleware' => ['auth:sanctum']], function () {
+    Route::post('login', [AuthController::class, 'signin']);
+    Route::post('register', [AuthController::class, 'signup']);
+    Route::resource('cards', CardsController::class);
 });
