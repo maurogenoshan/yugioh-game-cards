c431ffc2c22f35c38eaf90d1c2f78cb1e167468f
maurogaitan <maurogaitansouvaje@gmail.com>
2022-01-31 01:13:45 -0300

    Agrego metodo para subir imagenes

--- app/Http/Controllers/api/CardsController.php
+++ app/Http/Controllers/api/CardsController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\API\BaseController as BaseController;
 use App\Models\Card;
 use App\Http\Resources\Card as CardResource;
-
+use Illuminate\Support\Str;
 use App\Http\Requests\CardStoreRequest;
 
 
@@ -15,13 +15,26 @@ class CardsController extends BaseController
 
     public function index(Request $request)
     {
-        return CardResource::collection(Card::filterPaginate($request));
+        $card = new Card;
+        return CardResource::collection($card->filterPaginate($request));
     }
 
     public function store(CardStoreRequest $request)
     {
         $inputs = $request->validated();
+        $inputs['image'] = $this->uploadImage($request);
         return $this->sendResponse(new CardResource(Card::create($inputs)), 'Post created.');
+    }
+
+    public function uploadImage($request)
+    {
+        if (!$request->hasFile("image")) return;
+
+        $image = $request->file("image");
+        $nombreimagen = Str::slug($request->name) . "." . $image->guessExtension();
+        $ruta = public_path("img/post/");
+        copy($image->getRealPath(), $ruta . $nombreimagen);
+        return $nombreimagen;
     }
 
     public function show($id)
