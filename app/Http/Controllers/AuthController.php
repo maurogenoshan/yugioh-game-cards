30b336b7530ac8f1296da907358434a86e589211
maurogaitan <maurogaitansouvaje@gmail.com>
2022-02-11 03:17:41 -0300

    Registro y login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+   
+namespace App\Http\Controllers;
+   
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthRegisterequest;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+   
+class AuthController extends Controller
+{
+
+    public function signin(Request $request)
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            $authUser = Auth::user(); 
+            $success['token'] =  $authUser->createToken('MyAuthApp')->plainTextToken; 
+            $success['name'] =  $authUser->name;
+   
+            return $this->sendResponse($success, 'User signed in');
+        } 
+        else{ 
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        } 
+    }
+
+    public function signup(AuthRegisterequest $request)
+    {
+        $input = $request->validate();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyAuthApp')->plainTextToken;
+        $success['name'] =  $user->name;
+   
+        return $this->sendResponse($success, 'User created successfully.');
+    }
+   
+}
