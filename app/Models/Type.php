0c6623dbcd8ecc788f0d10b6f515fffd6d4717d3
maurogaitan <maurogaitansouvaje@gmail.com>
2022-02-10 23:09:00 -0300

    Refactor de controlador

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+
+    public function subtype()
+    {
+
+        return $this->hasMany('App\Type', 'parent_id');
+    }
+
+    public function scopeHasParent($query)
+    {
+        return $query->where('parent_id', '=', 0);
+    }
+
+    public function scopeFilterParent($query, $type)
+    {
+        return $query->where('name', '=', $type);
+    }
 }
